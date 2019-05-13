98ace91ae6cae59e59e613a34dc0ebdc0c281627
Daniel Shu <shudaniel95@gmail.com>
2019-05-13 00:10:38 -0400

    Delete store.js

--- src/store.js
+++
@@ -1,5 +0,0 @@
-import { createStore } from "redux";
-import reducer from "./reducers/reducer";
-import { composeWithDevTools } from "redux-devtools-extension";
-
-export default createStore(reducer, composeWithDevTools());
